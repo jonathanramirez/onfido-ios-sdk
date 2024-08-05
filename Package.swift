880c93a95d2814b5dbdad4628792c9f7fb177bd7
Jonathan Ramirez <jonathan.ramirez@check24.de>
2024-08-05 11:45:32 +0200

    update url

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         
             .binaryTarget(
                 name: "Onfido",
-                url: "https://github.com/jonathanramirez/Onfido/raw/main/Onfido.xcframework.zip",
+                url: "https://tmpfiles.org/dl/10619300/onfido.xcframework.zip",
                 checksum: "da6975d6bd945d1315c1e59e9a81b8c674de7abb9dc73ba29b1847ea0c275fcc"
             ),
         
